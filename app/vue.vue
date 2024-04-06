8d5279c602d448545c8f34945b7352e38cf39d33
unknown <chocomilkplz@gmail.com>
2024-04-06 18:42:11 -0400

    picture changes

---
+++ app/vue.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  const should_render = ref(true)
+
+ doSomething()
+</script>
+
+<template lang="pug">
+h3(v-if="shouldRender" @click="changeTitle" class="page-title") {{ pageTitle }}
+  p List below
+  ul
+    li(v-for="item in items" :key="item.id") {{ item.label }}
+</template>
